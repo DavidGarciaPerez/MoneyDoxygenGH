6faf26c375ac3edf9334c9a2daeb1870ac40a668
david.g.p <david.g.p@opendeusto.es>
2018-05-15 11:11:46 +0200

    Prueba de doxygen con generaciónde /doc
    Signed-off-by: david.g.p <david.g.p@opendeusto.es>

---
+++ docs/search/searchdata.js
@@ -0,0 +1,27 @@
+var indexSectionsWithContent =
+{
+  0: "acehimnst",
+  1: "im",
+  2: "e",
+  3: "im",
+  4: "acehimnst"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "files",
+  4: "functions"
+};
+
+var indexSectionLabels =
+{
+  0: "Todo",
+  1: "Clases",
+  2: "Namespaces",
+  3: "Archivos",
+  4: "Funciones"
+};
+
